9a7658f179df74f186a1a56797eb3bd02be1a622
Nathan Hollis <natehollis@gmail.com>
2015-12-30 00:44:08 -0600

    Add untracked files

---
+++ src/ToTypeScriptD.Lexical/Extensions/Extensions.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToTypeScriptD.Core.Extensions;
+
+namespace ToTypeScriptD.Lexical.Extensions
+{
+
+    public static class Extensions
+    {
+
+        [System.Diagnostics.DebuggerHidden]
+        public static string FormatWith(this string format, params object[] args)
+        {
+            return string.Format(System.Globalization.CultureInfo.CurrentCulture, format, args);
+        }
+
+
+        // TODO: look to move this to the WinMDExtensions.cs
+        public static string ToTypeScriptItemNameWinMD(this Type typeReference)
+        {
+            // Nested classes don't report their namespace. So we have to walk up the 
+            // DeclaringType tree to find the root most type to grab it's namespace.
+            var parentMostType = typeReference;
+            while (parentMostType.DeclaringType != null)
+            {
+                parentMostType = parentMostType.DeclaringType;
+            }
+
+            var mainTypeName = typeReference.FullName;
+
+            // trim namespace off of the front.
+            mainTypeName = mainTypeName.Substring(parentMostType.Namespace.Length + 1);
+
+            // replace the nested class slash with an underscore
+            mainTypeName = mainTypeName.Replace("/", "_").StripGenericTick();
+
+            mainTypeName = mainTypeName.StripGenericTick();
+            return mainTypeName;
+        }
+        
+        static Dictionary<string, string> _specialEnumNames = new Dictionary<string, string>
+        {
+            {"GB2312", "gb2312"},
+            {"PC437", "pc437"},
+            {"NKo", "nko"},
+        };
+
+        // Copied and modified from Json.Net
+        // https://github.com/JamesNK/Newtonsoft.Json/blob/master/Src/Newtonsoft.Json/Utilities/StringUtils.cs
+        public static string ToCamelCase(this string s, bool camelCaseConfig)
+        {
+            if (!camelCaseConfig)
+                return s;
+
+            if (_specialEnumNames.ContainsKey(s))
+            {
+                return _specialEnumNames[s];
+            }
+
+            if (string.IsNullOrEmpty(s))
+                return s;
+
+            if (!char.IsUpper(s[0]))
+                return s;
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < s.Length; i++)
+            {
+                bool hasNext = (i + 1 < s.Length);
+                if ((i == 0 || !hasNext) || char.IsUpper(s[i + 1]))
+                {
+                    char lowerCase;
+#if !(NETFX_CORE || PORTABLE)
+                    lowerCase = char.ToLower(s[i], System.Globalization.CultureInfo.InvariantCulture);
+#else
+                    lowerCase = char.ToLower(s[i]);
+#endif
+
+                    sb.Append(lowerCase);
+                }
+                else
+                {
+                    sb.Append(s.Substring(i));
+                    break;
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public static string StripGenericTick(this string value)
+        {
+            4.Times().Each(x =>
+            {
+                value = value.Replace("`" + x, "");
+            });
+            return value;
+        }
+        
+    }
+}
