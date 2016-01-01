f3b06588185495e9723cccf0c8afd6ae751d6f70
nh43de <natehollis@gmail.com>
2016-01-01 16:16:19 -0600

    TS Class extends

--- src/ToTypeScriptD.Core/TypeScript/TSClass.cs
+++ src/ToTypeScriptD.Core/TypeScript/TSClass.cs
@@ -10,13 +10,16 @@ namespace ToTypeScriptD.Core.TypeScript
         public bool IsExport { get; set; }
         public ICollection<TSMethod> Methods { get; set; } = new List<TSMethod>();
         public ICollection<TSField> Fields { get; set; } = new List<TSField>(); 
+        public ICollection<TSType> BaseTypes { get; set; } = new List<TSType>(); 
+
         public override string ToString()
         {
             var exportStr = IsExport ? "export " : "";
             var methods = string.Join("\r\n\r\n", Methods.Select(m => m.ToString()));
             var fields = string.Join("\r\n", Fields.Select(f => f.ToString() + ";"));
+            var extends = BaseTypes.Any() ? " extends " + string.Join(", ", BaseTypes.Select(b => b.ToString())) : "";
 
-            return $"{exportStr}class {Name}" + Environment.NewLine +
+            return $"{exportStr}class {Name}{extends}" + Environment.NewLine +
                    @"{" + Environment.NewLine +
                    $"{fields.Indent(TSFormattingConfig.IndentSpaces)}" + Environment.NewLine +
                    @"" + Environment.NewLine +
