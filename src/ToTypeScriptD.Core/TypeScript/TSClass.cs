eeba9bdf96719c50a74e26446dfe10382d15b129
nh43de <natehollis@gmail.com>
2016-01-02 03:17:08 -0600

    nested types

--- src/ToTypeScriptD.Core/TypeScript/TSClass.cs
+++ src/ToTypeScriptD.Core/TypeScript/TSClass.cs
@@ -7,6 +7,8 @@ namespace ToTypeScriptD.Core.TypeScript
 {
     public class TSClass : TSInterface
     {
+        //TODO: replace \r\n to config 
+
         public ICollection<PrimaryTypeScriptType> NestedClasses { get; set; } = new List<PrimaryTypeScriptType>(); 
 
         public override string ToString()
@@ -15,35 +17,38 @@ namespace ToTypeScriptD.Core.TypeScript
             var extends = BaseTypes.Any() ? " extends " + string.Join(", ", BaseTypes.Select(b => b.ToString())) : "";
             var generics = GenericParameters.Any() ? $" <{string.Join(", ", GenericParameters.Select(g => g.ToString()))}>" : "";
 
-            var methods = string.Join("\r\n\r\n", Methods.Select(m => m.ToString()));
+            var methods = string.Join("\r\n\r\n", Methods.Select(m => m + ""));
             if (!string.IsNullOrWhiteSpace(methods))
-                methods = methods.Indent(TSFormattingConfig.IndentSpaces) + Environment.NewLine;
+                methods = methods.Indent(TSFormattingConfig.IndentSpaces) + "\r\n";
 
-            var fields = string.Join("\r\n", Fields.Select(f => f.ToString() + ";"));
+            var fields = string.Join("\r\n", Fields.Select(f => f + ";"));
             if (!string.IsNullOrWhiteSpace(fields))
-                fields = fields.Indent(TSFormattingConfig.IndentSpaces) + Environment.NewLine;
+                fields = fields.Indent(TSFormattingConfig.IndentSpaces) + "\r\n";
 
-            var properties = string.Join("\r\n", Properties.Select(p => p.ToString() + ";"));
+            var properties = string.Join("\r\n", Properties.Select(p => p + ";"));
             if (!string.IsNullOrWhiteSpace(properties))
-                properties = properties.Indent(TSFormattingConfig.IndentSpaces) + Environment.NewLine;
+                properties = properties.Indent(TSFormattingConfig.IndentSpaces) + "\r\n";
 
-            var events = string.Join("\r\n", Events.Select(p => p.ToString() + ";"));
+            var events = string.Join("\r\n", Events.Select(p => p + ";"));
             if (!string.IsNullOrWhiteSpace(events))
-                events = events.Indent(TSFormattingConfig.IndentSpaces) + Environment.NewLine;
-            
-            var nestedClasses = string.Join("\r\n\r\n", NestedClasses.Select(n => n.ToString()));
-            if (!string.IsNullOrWhiteSpace(nestedClasses))
-                nestedClasses = nestedClasses.Indent(TSFormattingConfig.IndentSpaces) + Environment.NewLine;
+                events = events.Indent(TSFormattingConfig.IndentSpaces) + "\r\n";
 
+            
 
             var body = string.Join("\r\n",
-                new[] { fields, properties, events, methods, nestedClasses }
+                new[] { fields, properties, events, methods }
                     .Where(s => !string.IsNullOrWhiteSpace(s)));
 
-            return $"{exportStr}interface {Name}{generics}{extends}" + Environment.NewLine +
-                   @"{" + Environment.NewLine +
+            var nestedClasses = string.Join("\r\n\r\n", NestedClasses.Select(n => n + ""));
+            if (!string.IsNullOrWhiteSpace(nestedClasses))
+                nestedClasses = "\r\n\r\n" + nestedClasses;
+
+            //TODO: config for brackets on same line as declaration
+            return $"{exportStr}interface {Name}{generics}{extends}" + "\r\n" +
+                   @"{" + "\r\n" +
                    body +
-                   @"}";
+                   @"}" +
+                   nestedClasses;
         }
     }
 }
