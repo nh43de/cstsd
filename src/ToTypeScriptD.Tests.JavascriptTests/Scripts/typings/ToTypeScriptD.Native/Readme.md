00277e15b3af0361a47a3901eb93d8463194086d
nh43de <natehollis@gmail.com>
2016-09-28 10:13:27 -0500

    Project renames/cleanup

--- src/ToTypeScriptD.Tests.JavascriptTests/Scripts/typings/ToTypeScriptD.Native/Readme.md
+++
@@ -1,1 +0,0 @@
-The BeforeBuild build step on the JavaScript tests project should execute a command that generate a `.d.ts` file and sticks it here. (If all is good)
--- src/ToTypeScriptD.Tests.JavascriptTests/definitions.ts
+++
@@ -1,8 +0,0 @@
-﻿/// <reference path="Scripts/typings/ToTypeScriptD.Native/ToTypeScriptD.Native.d.ts" />
-/// <reference path="Scripts/typings/chai/chai-assert.d.ts" />
-/// <reference path="Scripts/typings/chai/chai.d.ts" />
-/// <reference path="Scripts/typings/mocha/mocha.d.ts" />
-
-// stubbing out WinJS type definitions for now
-
-declare var WinJS: any;
